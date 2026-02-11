2395c7e050524c97afbed718d15e8a860834056a
karamanghazal <qaramanghazal@gmail.com>
2026-02-11 21:19:37 +0300

    Delet Hallo world

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
         int age = 19;
         System.out.println("My age is "+age);
     }
