2a8d3abaa36dae620bcf75f59acb9590d79f5ecd
karamanghazal <qaramanghazal@gmail.com>
2026-02-11 21:17:10 +0300

    Add City in ghazal2 branch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       String city = "Amman";
+       System.out.println("I live in "+city);
+
     }
 }
